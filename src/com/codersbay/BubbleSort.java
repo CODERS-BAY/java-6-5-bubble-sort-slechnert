91697aeb4ac57eab7f15d4758f5e5a62e67fbf92
Simon <s.lechner.biz@gmail.com>
2020-02-27 09:08:29 +0100

    tmp initialized in loop, early skip condition added

--- src/com/codersbay/BubbleSort.java
+++ src/com/codersbay/BubbleSort.java
@@ -7,12 +7,14 @@ public class BubbleSort {
 
         int[] numbers = {45, 328, 13, 1, 886, 4, 23, 753523521};
         int lengthUnsorted = numbers.length;
-        int tmp;
+
 
         for (int i = 0; i < lengthUnsorted; i++) {
             for (int j = 1; j < lengthUnsorted && j >= 0; j++) {
-                if (numbers[j] < numbers[j - 1]) {
-                    tmp = numbers[j - 1];
+                if (numbers[j] > numbers[j - 1]) {
+                    continue;
+                } else {
+                    int tmp = numbers[j - 1];
                     numbers[j - 1] = numbers[j];
                     numbers[j] = tmp;
                 }
