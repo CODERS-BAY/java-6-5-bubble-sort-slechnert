9cc75ccf369c32caf29e77c19ef08cc52da986c9
Simon <s.lechner.biz@gmail.com>
2020-03-02 13:46:17 +0100

    bubble sort method implemented

--- src/com/codersbay/BubbleSort.java
+++ src/com/codersbay/BubbleSort.java
@@ -6,9 +6,14 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         int[] numbers = {45, 328, 13, 1, 886, 4, 23, 753523521};
+
+        int[] sortedNumbers = bubbleSort(numbers);
+        System.out.println(Arrays.toString(sortedNumbers));
+
+    }
+
+    public static int[] bubbleSort(int[] numbers) {
         int lengthUnsorted = numbers.length;
-
-
         for (int i = 0; i < lengthUnsorted; i++) {
             for (int j = 1; j < lengthUnsorted && j >= 0; j++) {
                 if (numbers[j] > numbers[j - 1]) {
@@ -20,7 +25,8 @@ public class BubbleSort {
                 }
             }
         }
-        System.out.println(Arrays.toString(numbers));
+        return numbers;
     }
+
 
 }
